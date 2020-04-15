8ead7a458f74df6626e74545b0a2a59d8b1075ca
Karol Kozuch <karol199714@gmail.com>
2020-04-16 00:02:24 +0200

    Added prototype moving code

---
+++ Assets/Scripts/Character/CharacterController.cs
@@ -0,0 +1,230 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+
+    //--Private Variables Exposed to the Inspector.
+    [SerializeField]
+    private float movementSpeed;
+    [SerializeField]
+    private float groundCheckRadius;
+    [SerializeField]
+    private float jumpForce;
+    [SerializeField]
+    private float slopeCheckDistance;
+    [SerializeField]
+    private float maxSlopeAngle;
+
+    [SerializeField]
+    private Transform groundCheck;
+
+    [SerializeField]
+    private LayerMask whatIsGround;
+
+    [SerializeField]
+    private PhysicsMaterial2D noFriction;
+    [SerializeField]
+    private PhysicsMaterial2D fullFritcion;
+
+    //--Private Variables
+    private float xInput;
+    private float slopeDownAngle;
+    private float slopeDownAngleOld;
+    private float slopeSideAngle;
+
+    private int facingDirection = 1;
+
+    private bool isGrounded;
+    private bool canJump;
+    private bool isOnSlope;
+    private bool isJumping;
+    private bool canWalkOnSlope;
+
+    private Vector2 newVelocity;
+    private Vector2 newForce;
+    private Vector2 colliderSize;
+    //Perpendicular
+    private Vector2 slopeNormalPerp;
+
+    //--Component References
+    private Rigidbody2D rb;
+    private CapsuleCollider2D cc;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        cc = GetComponent<CapsuleCollider2D>();
+
+        colliderSize = cc.size;
+    }
+
+    private void Update()
+    {
+        CheckInput();
+    }
+
+    private void FixedUpdate()
+    {
+        CheckGround();
+        SlopeCheck();
+        ApplyMovement();
+    }
+
+    private void SlopeCheck()
+    {
+        float scaledColliderSize = (colliderSize.y * transform.localScale.y);
+        Vector2 checkPos = transform.position - new Vector3(0, scaledColliderSize / 2, 0);
+
+        SlopeCheckHorizontal(checkPos);
+        SlopeCheckVertical(checkPos);
+
+    }
+
+    private void SlopeCheckHorizontal(Vector2 checkPos)
+    {
+        var slopeHitFront = Physics2D.Raycast(checkPos, transform.right, slopeCheckDistance, whatIsGround);
+        var slopeHitBack = Physics2D.Raycast(checkPos, -transform.right, slopeCheckDistance, whatIsGround);
+
+        if (slopeHitFront)
+        {
+            isOnSlope = true;
+            slopeSideAngle = Vector2.Angle(slopeHitFront.normal, Vector2.up);
+        }
+        else if (slopeHitBack)
+        {
+            isOnSlope = true;
+            slopeSideAngle = Vector2.Angle(slopeHitBack.normal, Vector2.up);
+        }
+        else
+        {
+            slopeSideAngle = 0.0f;
+            isOnSlope = false;
+        }
+    }
+
+    private void SlopeCheckVertical(Vector2 checkPos)
+    {
+        var hit = Physics2D.Raycast(checkPos, Vector2.down, slopeCheckDistance, whatIsGround);
+        Debug.DrawRay(checkPos, Vector2.down * slopeCheckDistance, Color.black);
+        if (hit)
+        {
+            slopeNormalPerp = Vector2.Perpendicular(hit.normal).normalized;
+
+            slopeDownAngle = Vector2.Angle(hit.normal, Vector2.up);
+
+            if (slopeDownAngle != slopeDownAngleOld)
+            {
+                isOnSlope = true;
+            }
+
+            slopeDownAngleOld = slopeDownAngle;
+
+            Debug.DrawRay(hit.point, slopeNormalPerp, Color.red);
+            Debug.DrawRay(hit.point, hit.normal, Color.yellow);
+        }
+
+        if (slopeDownAngle > maxSlopeAngle || slopeSideAngle > maxSlopeAngle)
+        {
+            canWalkOnSlope = false;
+        }
+        else
+        {
+            canWalkOnSlope = true;
+        }
+
+        if (isOnSlope && xInput == 0.0f && canWalkOnSlope)
+        {
+            rb.sharedMaterial = fullFritcion;
+        }
+        else
+        {
+            rb.sharedMaterial = noFriction;
+        }
+    }
+
+    private void CheckInput()
+    {
+        xInput = Input.GetAxisRaw("Horizontal");
+
+        if (xInput == -facingDirection)
+        {
+            Flip();
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+    }
+
+    private void CheckGround()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+
+        if (rb.velocity.y <= 0.0f)
+        {
+            isJumping = false;
+        }
+        if (isGrounded && !isJumping && slopeDownAngle <= maxSlopeAngle)
+        {
+            canJump = true;
+        }
+        else
+        {
+            canJump = false;
+        }
+    }
+
+    private void Jump()
+    {
+        if (canJump)
+        {
+            canJump = false;
+            isJumping = true;
+
+            newVelocity.Set(0.0f, 0.0f);
+            rb.velocity = newVelocity;
+            newForce.Set(0.0f, jumpForce);
+            rb.AddForce(newForce, ForceMode2D.Impulse);
+        }
+    }
+
+
+
+    private void ApplyMovement()
+    {
+        if (isGrounded && !isOnSlope && !isJumping)
+        {
+            //On flat ground
+            newVelocity.Set(movementSpeed * xInput, 0.0f);
+            rb.velocity = newVelocity;
+        }
+        else if (isGrounded && isOnSlope && !isJumping && canWalkOnSlope)
+        {
+            //On slope
+            //-xInput since the normal is rotated counterclockwise
+            newVelocity.Set(movementSpeed * slopeNormalPerp.x * -xInput, movementSpeed * slopeNormalPerp.y * -xInput);
+            rb.velocity = newVelocity;
+        }
+        else if (!isGrounded)
+        {
+            //In the air
+            newVelocity.Set(movementSpeed * xInput, rb.velocity.y);
+            rb.velocity = newVelocity;
+        }
+    }
+
+    private void Flip()
+    {
+        facingDirection *= -1;
+        transform.Rotate(0.0f, 180.0f, 0.0f);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+    }
+
+}
---
+++ Assets/Scripts/Common/Enums/FacingDirectionEnum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.Common.Enums
+{
+    /// <summary>
+    /// Defines directions that entities can face in the game.
+    /// </summary>
+    public enum FacingDirectionEnum
+    {
+        Left = 0,
+        Right = 1
+    }
+}
