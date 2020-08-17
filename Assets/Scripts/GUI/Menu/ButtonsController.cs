10a2e97abb87d281ae5c693eafbd9a4b440a4018
Karol Kozuch <karol199714@gmail.com>
2020-08-17 17:48:56 +0200

    WIP adding menu. Created menu bakground, added controls.

---
+++ Assets/Scripts/GUI/Menu/ButtonsController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.GUI.Menu
+{
+    /// <summary>
+    /// Manages buttons that are children of this scripts monobehavior.
+    /// </summary>
+    public class ButtonsController : MonoBehaviour
+    {
+        /// <summary>
+        /// Amount of buttons connected to this controller. Iterated from 0!
+        /// </summary>
+        private int buttonsCount;
+        /// <summary>
+        /// Stores all buttons connected to this control, together with their IDs. All of them.
+        /// </summary>
+        private Dictionary<int, CustomButton> _buttons = new Dictionary<int, CustomButton>();
+        // Start is called before the first frame update
+        void Start()
+        {
+            int buttonId = 0;
+
+            var buttons = GetComponentsInChildren<CustomButton>();
+            foreach (var button in buttons)
+            {
+                _buttons.Add(buttonId, button);
+                buttonId++;
+            }
+        }
+        /// <summary>
+        /// Checks if the player moved the selection among the buttons with use of vertical axis.
+        /// </summary>
+        private void ChkPlayerInput()
+        {
+
+        }
+        void Update()
+        {
+            
+        }
+    }
+}
---
+++ Assets/Scripts/GUI/Menu/CustomButton.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.GUI.Menu
+{
+    /// <summary>
+    /// Manages single button.
+    /// </summary>
+    public class CustomButton : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
