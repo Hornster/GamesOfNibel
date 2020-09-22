58ace45231e42585016ac4c81b5997f3722adef7
Karol Kozuch <karol199714@gmail.com>
2020-09-22 20:45:59 +0200

    Installed Cinemachine package

---
+++ Assets/Scripts/Camera/StackedCamreaConfigurator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.Camera
+{
+    /// <summary>
+    /// Configures provided camera in such a way that it can be stacked on top of other cameras.
+    /// </summary>
+    public class StackedCamreaConfigurator : MonoBehaviour
+    {
+        [Header("Required references")] 
+        [Tooltip("Camera to configure.")]
+        private UnityEngine.Camera _camera;
+    }
+}
