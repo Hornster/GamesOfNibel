5b11f1dd736eccc17fddd3a44b93ad0b5a024bca
Karol Kozuch <karol199714@gmail.com>
2020-09-25 00:58:47 +0200

    WIP setting cameras for splitscreen mode

---
+++ Assets/Scripts/GUI/Camera/CameraRenderOutput.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using UnityEngine.Animations;
+using UnityEngine.Experimental.Rendering;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.GUI.Camera
+{
+    /// <summary>
+    /// Passes the render result of provided camera to provided UI raw image element.
+    /// </summary>
+    public class CameraRenderOutput : MonoBehaviour
+    {
+        [Tooltip("The image which will be showing the render performed by the camera..")]
+        [SerializeField]
+        private RawImage _renderOutput;
+        [Tooltip("The transform of the image that shows the render.")]
+        [SerializeField]
+        private RectTransform _renderOutputTransform;
+
+        //[Tooltip("The texture which will be holding the render of the camera.")]
+        //[SerializeField]
+        private RenderTexture _renderTexture;
+        private UnityEngine.Camera _renderingCamera;
+        private void Start()
+        {
+            _renderOutput.texture = _renderTexture;
+        }
+        /// <summary>
+        /// Sets the render source camera to provided one.
+        /// </summary>
+        /// <param name="camera"></param>
+        public void SetSourceCamera(UnityEngine.Camera camera)
+        {
+            var renderTexDesc = new RenderTextureDescriptor(3840,2160,GraphicsFormat.R8G8B8A8_UNorm, (int)DepthBits.Depth32);
+            
+            _renderTexture = new RenderTexture(renderTexDesc);
+            camera.targetTexture = _renderTexture;
+            _renderingCamera = camera;
+        }
+
+        /// <summary>
+        /// Sets the size of the image on canvas using provided scale.
+        /// </summary>
+        /// <param name="sizeX">Size across X axis.</param>
+        /// <param name="sizeY">Size across Y axis.</param>
+        public void SetRenderOutputSize(int sizeX, int sizeY)
+        {
+            _renderOutputTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, sizeX);
+            _renderOutputTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, sizeY);
+        }
+
+        /// <summary>
+        /// Sets the position of the image on canvas. Render output images should have their
+        /// pivots set to upper left corner.
+        /// </summary>
+        /// <param name="positionX">Position across the X axis.</param>
+        public void SetRenderOutputPosition(float positionX)
+        {
+            _renderOutput.transform.localPosition = new Vector2(positionX, 0f);
+        }
+        /// <summary>
+        /// Sets the viewport of the camera.
+        /// </summary>
+        /// <param name="viewPortWidth">Width of the viewport, from 0 to 1.</param>
+        public void SetCameraViewport(float viewPortWidth)
+        {
+            viewPortWidth = Mathf.Clamp(viewPortWidth, 0f, 1f);
+
+            var cameraRect = _renderingCamera.rect;
+            cameraRect.width = viewPortWidth;
+            _renderingCamera.rect = cameraRect;
+        }
+    }
+}
---
+++ Assets/Scripts/GUI/Camera/SplitScreenRenderController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditorInternal;
+using UnityEngine;
+using UnityEngine.Animations;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.GUI.Camera
+{
+    /// <summary>
+    /// Setting the rendering cameras for split-screen mode.
+    /// </summary>
+    public class SplitScreenRenderController : MonoBehaviour
+    {
+        /// <summary>
+        /// Camera that renders entire screen to players, together with shared menu (pause menu, for example).
+        /// </summary>
+        [Tooltip("Camera that renders final image to the users, together with shared UI.")]
+        [SerializeField]
+        private UnityEngine.Camera _mainCamera;
+        [Tooltip("Main canvas showing merged UI.")]
+        [SerializeField]
+        private RectTransform _mainCanvas;
+        [Tooltip("Prefab for camera render output that is used to merge cameras from multiple players.")]
+        [SerializeField]
+        private GameObject _cameraRenderOutputPrefab;
+        [Tooltip("Parent GO for created camera render outputs.")]
+        [SerializeField]
+        private Transform _cameraRenderOutputParent;
+        [Tooltip("Used to divide the left and right halves of screen.")]
+        [SerializeField]
+        private Image _screenVerticalDivisor;
+        /// <summary>
+        /// Max amount of players in split-screen mode.
+        /// </summary>
+        private const int MaxPlayers = 2;
+        /// <summary>
+        /// Stores all created render outputs.
+        /// </summary>
+        private List<CameraRenderOutput> _registeredRenderOutputs = new List<CameraRenderOutput>();
+        /// <summary>
+        /// The instance for this singleton.
+        /// </summary>
+        private static SplitScreenRenderController _instance;
+
+        public static SplitScreenRenderController GetInstance()
+        {
+            if (_instance == null)
+            {
+                throw new Exception("Tried to retrieve instance of this singleton without creating the singleton!");
+            }
+
+            return _instance;
+       }
+        /// <summary>
+        /// Used to register new player in split-screen mode.
+        /// </summary>
+        /// <param name="playerCamera"></param>
+        public void RegisterPlayer(UnityEngine.Camera playerCamera)
+        {
+            if (_registeredRenderOutputs.Count > MaxPlayers)
+            {
+                throw new Exception($"Current game version supports up to {MaxPlayers} players in split screen mode!");
+            }
+
+            var newRenderOutput = Instantiate(_cameraRenderOutputPrefab, _cameraRenderOutputParent);
+            var renderOutputScript = newRenderOutput.GetComponentInChildren<CameraRenderOutput>();
+
+            _registeredRenderOutputs.Add(renderOutputScript);
+
+            renderOutputScript.SetSourceCamera(playerCamera);
+
+            ReconfigureSize();
+        }
+        /// <summary>
+        /// Accordingly changes the rects and render images of the registered players, splitting the screen. Affects camera rects, too.
+        /// </summary>
+        private void ReconfigureSize()
+        {
+            var mainCanvasRectWidth = _mainCanvas.rect.width;
+            var mainCanvasRectHeight = _mainCanvas.rect.height;
+
+            mainCanvasRectWidth /= _registeredRenderOutputs.Count;
+
+            var cameraViewportWidth = 1f / _registeredRenderOutputs.Count;
+                
+            for (int i = 0; i < _registeredRenderOutputs.Count; i++)
+            {
+                _registeredRenderOutputs[i].SetRenderOutputSize((int)mainCanvasRectWidth, (int)mainCanvasRectHeight);
+                _registeredRenderOutputs[i].SetRenderOutputPosition(i* mainCanvasRectWidth);
+                _registeredRenderOutputs[i].SetCameraViewport(cameraViewportWidth);
+            }
+        }
+
+        private void Awake()
+        {
+            if (_instance != null)
+            {
+                throw new Exception("Instance of this singleton has already been declared in object hierarchy!");
+            }
+            _instance = this;
+        }
+
+    }
+}
+
+//TODO: Screen resolution changes on display, but two player rendering cameras render to rendertexture, not display.
+//TODO: Make a simple class that detects change in screen resolution, register events to it and replace the
+//TODO: render texture with new one. Like here: https://answers.unity.com/questions/969731/how-do-i-check-if-the-screengamewindow-has-changed.html
