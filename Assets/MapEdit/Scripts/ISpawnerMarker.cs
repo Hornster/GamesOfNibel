60bde90014354e6a098f932feeaae7ebcebae06f
Karol Kozuch <karol199714@gmail.com>
2020-11-25 00:05:38 +0100

    Character factory now takes into account player team. Added map markers.

---
+++ Assets/MapEdit/Scripts/ISpawnerMarker.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.MapEdit.Scripts
+{
+    /// <summary>
+    /// Interface used for finding spawner positions on maps (so far, might change later).
+    /// </summary>
+    public interface ISpawnerMarker
+    {
+    }
+}
---
+++ Assets/MapEdit/Scripts/SpawnerMarkerScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Assets.MapEdit.Scripts
+{
+    /// <summary>
+    /// Script solely used for finding spawner positions on maps (so far, might change later).
+    /// </summary>
+    public class SpawnerMarkerScript : MonoBehaviour, ISpawnerMarker
+    {
+
+    }
+}
