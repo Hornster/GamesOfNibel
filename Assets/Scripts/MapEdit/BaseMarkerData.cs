e1ffe219f600a24b289a872c28cc8766b38f4dcb
Karol Kozuch <karol199714@gmail.com>
2021-06-08 01:33:57 +0200

    322 - Created component prefabs for base markers. 317 - WIP component factory

---
+++ Assets/Scripts/MapEdit/BaseMarkerData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assets.Scripts.Game.Common;
+using Assets.Scripts.Game.Common.Enums;
+using UnityEngine;
+
+namespace Assets.Scripts.MapEdit
+{
+    public class BaseMarkerData : MonoBehaviour
+    {
+        private static int _lastUsedID = 0;
+        /// <summary>
+        /// Unique ID of the base
+        /// </summary>
+        public int ID { get; private set; }
+        [SerializeField]
+        private TeamModule _myTeam;
+        [SerializeField]
+        private GameplayModesEnum _gameMode;
+        [SerializeField]
+        private BaseSubtypeEnum _baseSubtype;
+
+        private void Start()
+        {
+            _lastUsedID += 1;
+            ID = _lastUsedID;
+        }
+    }
+}
