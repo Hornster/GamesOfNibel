6f74434f38524ea33edeb9df9498fe7a8685d2db
Karol Kozuch <karol199714@gmail.com>
2021-06-13 16:51:27 +0200

    WIP searching for bases in editor

--- Assets/Scripts/MapEdit/Editor/Data/ScriptableObjects/BaseMarkersCacheSO.cs
+++ Assets/Scripts/MapEdit/Editor/Data/ScriptableObjects/BaseMarkersCacheSO.cs
@@ -9,49 +9,25 @@ using Object = UnityEngine.Object;
 
 namespace Assets.Scripts.MapEdit.Editor.Data.ScriptableObjects
 {
-    [CreateAssetMenu(fileName = BaseMarkersCacheSOName, menuName = SGMapEditPaths.MapEditMenuPath + BaseMarkersCacheSOName)]
-    public class BaseMarkersCacheSO : ScriptableObject
+    public class BaseMarkersCacheSO : UnityEditor.Editor
     {
-        public const string BaseMarkersCacheSOName = "BaseMarkersCacheSO";
-        private List<GameObject> _knownBases = new List<GameObject>();
-
-        public void AddBase(GameObject baseMainGameObject)
+        private List<BaseMarkerData> _basesDatas = new List<BaseMarkerData>();
+        private bool BasesAdded = false;
+        public void FindBases()
         {
-            //TODO: This won't work. GameObjects cannot be stored in SO. Try out FindObjectByType instead
-            _knownBases.Add(baseMainGameObject);
-            EditorUtility.SetDirty(this);
+            var foundBaseMarkers = FindObjectsOfType<BaseMarkerData>();
+            _basesDatas = foundBaseMarkers.ToList();
         }
-        /// <summary>
-        /// Checks if base markers are still present. If given marker is not present
-        /// removes it from the list.
-        /// </summary>
-        public void ChkBases()
+        private void ChkReferences()
         {
-            for (int i = _knownBases.Count - 1; i >= 0; i--)
+            for(int i = _basesDatas.Count-1; i >= 0; i++)
             {
-                if (_knownBases[i] == null)
+                if (_basesDatas[i] == null)
                 {
-                    _knownBases.RemoveAt(i);
+                    _basesDatas.RemoveAt(i);
                 }
             }
-            EditorUtility.SetDirty(this);
         }
-        /// <summary>
-        /// Gets BaseMarkerData components from all known bases. Checks what bases are
-        /// still present beforehand.
-        /// </summary>
-        /// <returns></returns>
-        public List<BaseMarkerData> GetBasesData()
-        {
-            ChkBases();
-
-            var basesData = new List<BaseMarkerData>();
-            foreach (var knownBase in _knownBases)
-            {
-                basesData.Add(knownBase.GetComponent<BaseMarkerData>());
-            }
-
-            return basesData;
-        }
+        
     }
 }
