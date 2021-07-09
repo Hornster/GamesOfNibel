2f2cb5e47dcb03e68107826ea73d8e7ec45babbb
Karol Kozuch <karol199714@gmail.com>
2021-07-09 15:42:12 +0200

    syncing before deprature

---
+++ Assets/Scripts/Game/GameModes/Race/RaceBaseController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assets.Scripts.Game.Common.CustomEvents;
+using Assets.Scripts.Game.Player;
+using UnityEngine;
+
+namespace Assets.Scripts.Game.GameModes.Race
+{
+    [RequireComponent(typeof(BoxCollider2D))]
+    public class RaceBaseController : MonoBehaviour
+    {
+        private IntegerUnityEvent _playerArrived;
+
+        private void OnTriggerEnter2D(Collider2D collider)
+        {
+            
+
+        }
+
+        private void ChkForPlayer(Collider2D collider)
+        {
+            var playerController = collider.GetComponent<PlayerController>();
+            if (playerController == null)
+            {
+                return;
+            }
+
+
+        }
+    }
+}
